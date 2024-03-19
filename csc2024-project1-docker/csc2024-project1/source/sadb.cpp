105aee36504ed56c77a7fcb4466477a0a7aa849c
clai <charlielai.15@gmail.com>
2024-03-20 00:33:36 +0800

    Update sadb.cpp

--- csc2024-project1-docker/csc2024-project1/source/sadb.cpp
+++ csc2024-project1-docker/csc2024-project1/source/sadb.cpp
@@ -11,32 +11,50 @@ std::optional<ESPConfig> getConfigFromSADB() {
   std::vector<uint8_t> message(65536);
   sadb_msg msg{};
   // TODO: Fill sadb_msg
-  // msg.sadb_msg_version =
-  // msg.sadb_msg_type =
-  // msg.sadb_msg_satype =
-  // msg.sadb_msg_len =
-  // msg.sadb_msg_pid =
+  msg.sadb_msg_version = PF_KEY_V2;
+  msg.sadb_msg_type    = SADB_GET;
+  msg.sadb_msg_satype  = SADB_SATYPE_ESP;
+  msg.sadb_msg_len     = sizeof(sadb_msg) / 8;
+  msg.sadb_msg_pid     = getpid();
 
   // TODO: Create a PF_KEY_V2 socket and write msg to it
   // Then read from socket to get SADB information
+  int sock = socket(PF_KEY, SOCK_RAW, PF_KEY_V2);
+  write(sock, &msg, sizeof(sadb_msg));
+  read(sock, message.data(), message.size());
 
   // TODO: Set size to number of bytes in response message
-  int size = sizeof(sadb_msg);
+  int size = message.size();
 
   // Has SADB entry
   if (size != sizeof(sadb_msg)) {
     ESPConfig config{};
     // TODO: Parse SADB message
     // config.spi = 0x00000000;
+    config.spi  = reinterpret_cast<sadb_sa*>(message.data())->sadb_sa_spi;
+
     // config.aalg = std::make_unique<ESP_AALG>(ALGORITHM_ID, KEY);
+    config.aalg = std::make_unique<ESP_AALG>(reinterpret_cast<sadb_sa*>(message.data())->sadb_sa_encrypt, reinterpret_cast<sadb_sa*>(message.data())->sadb_sa_auth);
+
     // Have enc algorithm:
     //   config.ealg = std::make_unique<ESP_AALG>(ALGORITHM_ID, KEY);
     // No enc algorithm:
     //   config.ealg = std::make_unique<ESP_EALG>(SADB_EALG_NONE, std::span<uint8_t>{});
+    if(reinterpret_cast<sadb_sa*>(message.data())->sadb_sa_encrypt == SADB_EALG_NONE) {
+      config.ealg = std::make_unique<ESP_EALG>(SADB_EALG_NONE, std::span<uint8_t>{});
+    } 
+    else {
+      config.ealg = std::make_unique<ESP_EALG>(reinterpret_cast<sadb_sa*>(message.data())->sadb_sa_encrypt, reinterpret_cast<sadb_sa*>(message.data())->sadb_sa_auth);
+    }
+
     // Source address:
     //   config.local = ipToString(ADDR);
     // Destination address:
     //   config.remote = ipToString(ADDR);
+    
+    config.local  = ipToString(reinterpret_cast<iphdr*>(message.data())->saddr);
+    config.remote = ipToString(reinterpret_cast<iphdr*>(message.data())->daddr);
+
     return config;
   }
   std::cerr << "SADB entry not found." << std::endl;
